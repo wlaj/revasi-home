a401a7fee52c645f0af30b4a81ddc80fa0fa902a
wlaj <wlajansen16@gmail.com>
2025-04-19 22:05:39 +0200

    Add footer section, blocks section, testimonials section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,21 @@ import FeaturesSection from "@/components/features-section";
 import HeroSection from "@/components/hero-section";
 import IntegrationsSection from "@/components/integrations-section";
 import StatsSection from "@/components/stats-section";
-import CarouselSection from '@/components/carousel-section';
+import CarouselSection from "@/components/carousel-section";
+import BlocksSection from "@/components/blocks-section";
+import TestimonialsSection from "@/components/testimonials-section";
+import FooterSection from "@/components/footer-section";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
       <FeaturesSection />
-      <CarouselSection />
       <StatsSection />
-      <IntegrationsSection />
+      <CarouselSection />
+      <BlocksSection />
+      <TestimonialsSection />
+      <FooterSection />
     </main>
   );
 }
