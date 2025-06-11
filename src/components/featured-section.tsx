9887e4cc0329573448340b719431dbf5f83e0f58
wlaj <wlajansen16@gmail.com>
2025-06-11 19:41:11 +0200

    Add external url to card

--- src/components/featured-section.tsx
+++ src/components/featured-section.tsx
@@ -46,7 +46,7 @@ const FeaturedSection = () => {
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {featuredRestaurants.map((restaurant) => (
-            <FeaturedRestaurantCard key={restaurant.id} {...restaurant} />
+            <FeaturedRestaurantCard url={restaurant.title} key={restaurant.id} {...restaurant} />
           ))}
         </div>
       </div>
